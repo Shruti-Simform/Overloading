f2a1a1ddbf4530394593e24359b219db59cf0a13
Shruti <shruti@simformsolutions.com>
2023-04-03 17:06:43 +0530

    Modified

--- src/CalculateSum.java
+++ src/CalculateSum.java
@@ -1,43 +1,48 @@
 public class CalculateSum {
     //                  ----- CONSTRUCTOR OVERLOADING -----
-    CalculateSum(){
+    CalculateSum() {
         System.out.println("--- Calculate Sum ---");
     }
-    CalculateSum(int a, int b){
+
+    CalculateSum(int a, int b) {
         System.out.println("--- Calculate Sum of 2 integers ---");
     }
-    CalculateSum(double a, double b){
+
+    CalculateSum(double a, double b) {
         System.out.println("--- Calculate Sum of 2 double numbers ---");
     }
-    CalculateSum(int a, double b){
+
+    CalculateSum(int a, double b) {
         System.out.println("--- Calculate Sum of an integer and a double number ---");
     }
-    CalculateSum(double a, int b){
+
+    CalculateSum(double a, int b) {
         System.out.println("--- Calculate Sum of a double number and an integer ---");
     }
-
 
 
     //                  ----- THREE WAYS OF METHOD OVERLOADING -----
 
     //CHANGE IN NUMBER OF ARGUMENTS
-    int addition(int a, int b){
-        return a+b;
+    int addition(int a, int b) {
+        return a + b;
     }
-    int addition(int a, int b, int c){
-        return  a+b+c;
+
+    int addition(int a, int b, int c) {
+        return a + b + c;
     }
 
     //CHANGE IN DATATYPE
-    double addition(double a, double b){
-        return a+b;
+    double addition(double a, double b) {
+        return a + b;
     }
 
     //CHANGE IN ORDER OF DATATYPE
-    double addition(int a, double b){
-        return a+b;
+    double addition(int a, double b) {
+        return a + b;
     }
-    double addition(double a, int b){
-        return a+b;
+
+    double addition(double a, int b) {
+        return a + b;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,23 @@ public class Main {
         double result;
 
         // Add two integers
-        CalculateSum c1 = new CalculateSum(2,3);
-        res = c1.addition(2,3);
-        System.out.println("Addition of 1 and 2 : "+ res + "\n");
+        CalculateSum c1 = new CalculateSum(2, 3);
+        res = c1.addition(2, 3);
+        System.out.println("Addition of 1 and 2 : " + res + "\n");
 
         // Add two double numbers
         CalculateSum c2 = new CalculateSum(1.1, 2.2);
-        result = c2.addition(1.1,2.2);
-        System.out.println("Addition of 1.1 and 2.2 : "+ String.format("%.2f",result) + "\n");
+        result = c2.addition(1.1, 2.2);
+        System.out.println("Addition of 1.1 and 2.2 : " + String.format("%.2f", result) + "\n");
 
         // Add an integer and a double number
         CalculateSum c3 = new CalculateSum(1, 2.2);
-        result = c3.addition(1,2.2);
-        System.out.println("Addition of 1 and 2.2 : "+ result + "\n");
+        result = c3.addition(1, 2.2);
+        System.out.println("Addition of 1 and 2.2 : " + result + "\n");
 
         // Add a double number and an integer
         CalculateSum c4 = new CalculateSum(1.1, 2);
-        result = c4.addition(1.1,2);
-        System.out.println("Addition of 1.1 and 2 : "+ result +"\n");
+        result = c4.addition(1.1, 2);
+        System.out.println("Addition of 1.1 and 2 : " + result + "\n");
     }
 }
